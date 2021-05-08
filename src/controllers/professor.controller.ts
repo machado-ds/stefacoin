de1b2b563e015479dbde76baab9fd911431431c0
Davide Viana <davidjb.vr@gmail.com>
2021-05-08 13:18:04 -0300

    implementacao desafio final

---
+++ src/controllers/professor.controller.ts
@@ -0,0 +1,58 @@
+import Professor from '../entities/professor.entity';
+import ProfessorRepository from '../repositories/professor.repository';
+import { FilterQuery } from '../utils/database/database';
+import Mensagem from '../utils/mensagem';
+import { Validador } from '../utils/utils';
+
+export default class ProfessorController {
+  async obterPorId(id: number): Promise<Professor> {
+    Validador.validarParametros([{ id }]);
+
+    return await ProfessorRepository.obterPorId(id);
+  }
+
+  async obter(filtro: FilterQuery<Professor> = {}): Promise<Professor> {
+    return await ProfessorRepository.obter(filtro);
+  }
+
+  // #pegabandeira
+  async listar(filtro: FilterQuery<Professor> = {}): Promise<Professor[]> {
+    return await ProfessorRepository.listar(filtro);
+  }
+
+  // #pegabandeira
+  async incluir(professor: Professor) {
+    const { nome, email, senha } = professor;
+
+    Validador.validarParametros([{ nome }, { email }, { senha }]);
+    professor.tipo = 1;
+
+    const id = await ProfessorRepository.incluir(professor);
+
+    return new Mensagem('Professor incluido com sucesso!', {
+      id,
+    });
+  }
+
+  async alterar(id: number, professor: Professor) {
+    const { nome, email, senha } = professor;
+
+    Validador.validarParametros([{ id }, { nome }, { email }, { senha }]);
+
+    await ProfessorRepository.alterar({ id }, professor);
+
+    return new Mensagem('Professor alterado com sucesso!', {
+      id,
+    });
+  }
+
+  async excluir(id: number) {
+    Validador.validarParametros([{ id }]);
+
+    await ProfessorRepository.excluir({ id });
+
+    return new Mensagem('Professor excluido com sucesso!', {
+      id,
+    });
+  }
+}
--- src/models/transacao.model.js
+++
@@ -1,7 +0,0 @@
-export default class Transacao {
-  constructor(enderecoRemetente, enderecoDestinatario, valor) {
-    this.enderecoRemetente = enderecoRemetente;
-    this.enderecoDestinatario = enderecoDestinatario;
-    this.valor = valor;
-  }
-}
